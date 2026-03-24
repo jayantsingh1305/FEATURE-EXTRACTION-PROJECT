987722dfb27cdf409546d5e523fdd8d565bebe41
Jayant Singh <jayantofficial1305@gmail.com>
2026-03-24 15:11:56 +0530

    Update feature_extraction.py

--- feature_extraction.py
+++ feature_extraction.py
@@ -3,7 +3,7 @@ import numpy as np
 
 df = pd.read_csv('anime.csv')
 
-#EPISODE EXTRACTION
+#make a new column for episode count
 def extract_episodes(txt):
     check = False
     data = ""
@@ -20,7 +20,7 @@ df["Episodes"] = df["Title"].apply(extract_episodes)
 df["Episodes"] = df["Episodes"].str.replace("eps","")
 df["Episodes"] = df["Episodes"].astype(int)
 
-#TIME STAMP EXTRACTION
+#make a new column for the time stamp
 def extraction_time(txt):
     check = False
     data = ""
@@ -33,4 +33,38 @@ def extraction_time(txt):
         
 df['Total Time'] = df['Title'].apply(extraction_time)
 
+#which anime has the highest score 
+df["score"] = pd.to_numeric(df["Score"])
+df = df.dropna(subset=["Score"])
+max_score = df["Score"].max()
+top_rows = df[df["Score"] == max_score][["Rank","Title","Score"]]
+print("Max Score:", max_score)
+print(top_rows)
+
+#give me top 5 highesr scoring anime
+df_sorted = df.sort_values(by="Score", ascending=False)
+top5 = df_sorted.head(5)[["Rank","Title","Score"]]
+print(top5)
+
+#which anime has the highest episode count
+df["Episodes"] = pd.to_numeric(df["Episodes"],errors="coerce")
+df = df.dropna(subset=["Episodes"])
+
+top = df.loc[df["Episodes"].idxmax(),["Rank","Title","Score","Episodes"]]
+print(top)
+
+#animes with top 5 episode count
+top5_eps = (
+    df.sort_values(by=["Episodes", "Title"], ascending=[False, True])
+      .head(5)[["Rank", "Title", "Score", "Episodes"]]
+)
+print(top5_eps)
+
+#which is the longest running anime
+max_ep = df["Episodes"].max()
+longest_running = df[df["Episodes"] == max_ep][["Title", "Episodes", "Rank", "Score"]]
+print("Max Episodes:", max_ep)
+print(longest_running)
+
 print(df)
+
